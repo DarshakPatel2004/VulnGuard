0497bc340ced1081b10f83fffced7b456fb8ac78
DarshakPatel2004 <darshakpatel2004@gmail.com>
2026-04-05 21:11:24 +0530

    New Project

--- tmp/check_status.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-con = sqlite3.connect('vuln_tracker.db')
-cur = con.cursor()
-rows = cur.execute('SELECT source, last_fetched, last_run_status, error_message FROM fetchlog').fetchall()
-for r in rows:
-    print(f"Source: {r[0]}, Last Fetched: {r[1]}, Status: {r[2]}, Error: {r[3]}")
-con.close()
